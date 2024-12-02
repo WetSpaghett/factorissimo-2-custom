09f7da751bad28b45c40bf1bdd103d749fc777d6
WetSpaghett <43825810+WetSpaghett@users.noreply.github.com>
2024-12-02 15:06:19 +0000

    Remove unnecessary surface conditions for yumako and jellystem

--- prototypes/greenhouse.lua
+++ prototypes/greenhouse.lua
@@ -25,13 +25,3 @@ for _, tower in pairs(data.raw["agricultural-tower"]) do
         min = 25
     })
 end
-
-for _, plant in pairs{"jellystem", "yumako-tree"} do
-    plant = data.raw.plant[plant]
-    plant.surface_conditions = plant.surface_conditions or {}
-    table.insert(plant.surface_conditions, {
-        property = "pressure",
-        min = 2000,
-        max = 2000
-    })
-end
