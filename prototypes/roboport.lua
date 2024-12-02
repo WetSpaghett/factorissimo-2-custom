decb89773d255aa5c6bbd7fa14f033191b49b20e
WetSpaghett <43825810+WetSpaghett@users.noreply.github.com>
2024-12-02 15:50:52 +0000

    Update roboport.lua

--- prototypes/roboport.lua
+++ prototypes/roboport.lua
@@ -160,12 +160,12 @@ entities_to_extend[#entities_to_extend + 1] = roboport
 local storage_chest = table.deepcopy(data.raw["logistic-container"]["storage-chest"])
 storage_chest.name = "factory-construction-chest"
 storage_chest.inventory_type = "with_bar"
-storage_chest.icon = "__factorissimo-2-notnotmelon__/graphics/icon/construction-chest.png"
+storage_chest.icon = F .. "/graphics/icon/construction-chest.png"
 storage_chest.icon_size = 64
 storage_chest.inventory_size = 100
 storage_chest.hidden = true
 storage_chest.flags = {"player-creation", "placeable-player", "no-automated-item-removal", "no-automated-item-insertion", "not-on-map"}
-storage_chest.animation.layers[1].filename = "__factorissimo-2-notnotmelon__/graphics/entity/construction-chest.png"
+storage_chest.animation.layers[1].filename = F .. "/graphics/entity/construction-chest.png"
 entities_to_extend[#entities_to_extend + 1] = storage_chest
 
 for _, factory_name in pairs {"factory-1", "factory-2", "factory-3"} do
@@ -213,7 +213,7 @@ data:extend {{
 data:extend {{
     type = "item",
     name = "factory-construction-chest",
-    icon = "__factorissimo-2-notnotmelon__/graphics/icon/construction-chest.png",
+    icon = F .. "/graphics/icon/construction-chest.png",
     icon_size = 64,
     stack_size = 1,
     hidden = true,
